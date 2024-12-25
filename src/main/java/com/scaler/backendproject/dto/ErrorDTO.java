954670616fa8f6bd56f91ecb420a7a4e374b1a4d
Purusharth Agarwal <purusharthworkshere@gmail.com>
2024-12-25 18:51:16 +0530

    Minor Changes

--- src/main/java/com/scaler/backendproject/dto/ErrorDTO.java
+++ src/main/java/com/scaler/backendproject/dto/ErrorDTO.java
@@ -1,10 +1,14 @@
 package com.scaler.backendproject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDTO {
 
     public String getMessage() {
