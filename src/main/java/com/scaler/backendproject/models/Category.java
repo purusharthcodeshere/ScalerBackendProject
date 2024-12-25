4e8cdcb91af7a202d364372b9f78350390122630
Purusharth Agarwal <purusharthworkshere@gmail.com>
2024-12-26 00:58:50 +0530

    Fetch Types + Adding Spring Hibernate JPA Properties

--- src/main/java/com/scaler/backendproject/models/Category.java
+++ src/main/java/com/scaler/backendproject/models/Category.java
@@ -1,10 +1,14 @@
 package com.scaler.backendproject.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +17,37 @@ import lombok.Setter;
 @Entity
 public class Category extends BaseModel{
     private String title;
+
+    //We declare the cardinality between the Product and the Category Class
+    //We mention mappedBy here so that Hibernate
+    // does not create another mapping on its own
+    //Since in the Product class, this cardinality is mapped by the category attribute,
+    //we mention that here to indicate Hibernate not to create another mapping
+    //We also mention the fetch type b/w Eager and Lazy
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+    private List<Product> products;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "title='" + title + '\'' +
+                ", products=" + products +
+                '}';
+    }
 }
