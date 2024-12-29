78d68e3179704175a427d87cd181981a308369f4
Purusharth Agarwal <purusharthworkshere@gmail.com>
2024-12-29 21:30:33 +0530

    Added annotations for TimeStamps + Config file AuditJPA for createdBy Attribute

--- src/main/java/com/scaler/backendproject/BackendProjectApplication.java
+++ src/main/java/com/scaler/backendproject/BackendProjectApplication.java
@@ -1,11 +1,11 @@
 package com.scaler.backendproject;
 
-import com.scaler.backendproject.models.Category;
-import com.scaler.backendproject.models.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/scaler/backendproject/models/BaseModel.java
+++ src/main/java/com/scaler/backendproject/models/BaseModel.java
@@ -8,8 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,9 +25,14 @@ public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Date createdAt;
-    private Date updatedAt;
+    @CreatedDate
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
     private boolean isDeleted;
     //This (createdBy) was optional in the class
+    @CreatedBy
     private String createdBy;
 }
