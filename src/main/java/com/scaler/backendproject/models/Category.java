f98a6683d685bf01d9140a9ff1a98e551430cd01
Purusharth Agarwal <purusharthworkshere@gmail.com>
2024-12-29 21:33:29 +0530

    Added Annotations for EntityListener + Changes in List<Product> attribute

--- src/main/java/com/scaler/backendproject/models/Category.java
+++ src/main/java/com/scaler/backendproject/models/Category.java
@@ -1,20 +1,19 @@
 package com.scaler.backendproject.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.EntityListeners;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Category extends BaseModel {
     private String title;
 
@@ -24,8 +23,8 @@ public class Category extends BaseModel {
     //Since in the Product class, this cardinality is mapped by the category attribute,
     //we mention that here to indicate Hibernate not to create another mapping
     //We also mention the fetch type b/w Eager and Lazy
-    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
-    private List<Product> products;
+//    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+//    private List<Product> products;
 
     public String getTitle() {
         return title;
@@ -35,19 +34,18 @@ public class Category extends BaseModel {
         this.title = title;
     }
 
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
+//    public List<Product> getProducts() {
+//        return products;
+//    }
+//
+//    public void setProducts(List<Product> products) {
+//        this.products = products;
+//    }
 
     @Override
     public String toString() {
         return "Category{" +
                 "title='" + title + '\'' +
-                ", products=" + products +
                 '}';
     }
 }
--- src/main/java/com/scaler/backendproject/models/Product.java
+++ src/main/java/com/scaler/backendproject/models/Product.java
@@ -1,18 +1,20 @@
 package com.scaler.backendproject.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Product extends BaseModel {
     private String title;
     private String description;
