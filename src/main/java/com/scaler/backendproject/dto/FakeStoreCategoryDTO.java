fea9991a30f05ca37c0c9d96a16512e10aa8a435
Purusharth Agarwal <purusharthworkshere@gmail.com>
2025-03-26 21:26:31 +0530

    Minor Changes

--- src/main/java/com/scaler/backendproject/dto/FakeStoreCategoryDTO.java
+++ src/main/java/com/scaler/backendproject/dto/FakeStoreCategoryDTO.java
@@ -1,12 +1,14 @@
 package com.scaler.backendproject.dto;
 
 import com.scaler.backendproject.models.Category;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//@Getter
-//@Setter
+@Getter
+@Setter
 public class FakeStoreCategoryDTO {
     private Long id;
     private String title;
